dd4d46dbbf8f9e67aca4e0117dc25247c1e31cdf
Marie-Luise Lux <Marie-Luise.Lux@web.de>
2016-05-30 11:46:06 +0200

    working routes and updated application

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
+  root 'startup#index'
+
   resources :users
 
-  get 'actor/generate_Actor'
-
-  get 'startup/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
