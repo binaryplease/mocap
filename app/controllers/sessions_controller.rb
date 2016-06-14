56185a5e301021aba58643dd5b6dd790796178ef
SoRoot <lukas.ungerland@gmail.com>
2016-06-14 10:49:34 +0200

    css ajusments for login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,10 @@
 class SessionsController < ApplicationController
-
+	
 	def new
 	end
 
 	def create
-		@user = User.find_by_email(params[:session][:email])
+		@user = User.find_by_name(params[:session][:username])
 		if @user && @user.authenticate(params[:session][:password])
 			session[:user_id] = @user.id
 			redirect_to '/'
