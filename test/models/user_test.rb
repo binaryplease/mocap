4acda6647346f811ca7bf80b1e2f52ec42ac50a7
SoRoot <lukas.ungerland@gmail.com>
2016-06-20 16:40:38 +0200

    cleanup

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,11 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+	def setup
+		@user = User.new(username: "Example User", password: "secret", password_confirmation: "secret")
+	end
+
+	test "should be valid" do
+		assert @user.valid?
+	end
 end
