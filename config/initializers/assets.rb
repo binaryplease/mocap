b20f7effed498063116c9f0ea215ca0d70dafdf8
SoRoot <lukas.ungerland@gmail.com>
2016-06-20 17:33:18 +0200

    stylesheets in order

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,19 +3,6 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
-# Rails.application.config.assets.precompile += %w( dropdown.css )
-# Rails.application.config.assets.precompile += %w( greyscale.css )
-# Rails.application.config.assets.precompile += %w( greyscale.js )
-Rails.application.config.assets.precompile += %w( search.css )
-Rails.application.config.assets.precompile += %w( search.js )
-# Rails.application.config.assets.precompile += %w( bootstrap.min.css )
-Rails.application.config.assets.precompile += %w( users.css )
-Rails.application.config.assets.precompile += %w( page-scroll-annimation.js )
-# Rails.application.config.assets.precompile += %w( sessions.css )
-# Rails.application.config.assets.precompile += %w( greyscale_adjustments.css )
-# Rails.application.config.assets.precompile += %w( fancybox.css )
-
-	
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
